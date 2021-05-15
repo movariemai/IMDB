2d96c15d9a74290571ac63d216b241dcca18d388
movariemai <57597993+movariemai@users.noreply.github.com>
2021-05-15 10:22:31 +0300

    Add BornThisMonth page and functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,48 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using IMDB.Models;
+using MvcMovie.Models;
+using MvcMovie.Data;
+using MvcActor.Models;
+using MvcActor.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace IMDB.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MvcMovieContext _released;
+        private readonly MvcActorContext _born;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MvcMovieContext released, MvcActorContext born)
         {
             _logger = logger;
+            _released = released;
+            _born = born;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> BornThisMonth()
+        {
+            int currentMonth = DateTime.Now.Month;
+
+            var actors = from a in _born.Actor.Where(a => (a.Birthday).Month == currentMonth) orderby (a.Birthday).Day
+                    select a;
+
+            return View(await actors.ToListAsync());
+        }
+		
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
